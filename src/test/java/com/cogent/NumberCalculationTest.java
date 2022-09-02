bc3b825439b4b898b91196d0f71887202ecde413
FariaMansh <fariamansha786@gmail.com>
2022-09-02 17:48:06 -0400

    Junit_updated

--- src/test/java/com/cogent/NumberCalculationTest.java
+++ src/test/java/com/cogent/NumberCalculationTest.java
@@ -23,43 +23,43 @@ public class NumberCalculationTest {
 		assertEquals(nc.add(10, 20), 30);
 	}
 	
-//	//Inner Class
-//	@Nested
-//	class AddTest {
-//		
-//		@Test
-//		public void addPositive() {
-//			assertEquals(nc.add(2, 4), 6);
-//			
-//		}
-//		
-//		@Test
-//		public void addPos_Neg() {
-//			assertEquals(nc.add(9, -5), 4);
-//			
-//		}
-//		
-//		@Test
-//		public void addNeg() {
-//			assertEquals(-2, nc.add(-1, -1));
-//		}
-//	}
-//	
-//	
-//	@Test
-//	public void testMultiply() {
-//		assertAll(
-//				() -> assertEquals(0, nc.multiply(0, 1)),
-//				() -> assertEquals(5, nc.multiply(5, 1)),
-//				() -> assertEquals(nc.multiply(4, 5), 20)
-//				);
-//	}
+	//Inner Class
+	@Nested
+	class AddTest {
+		
+		@Test
+		public void addPositive() {
+			assertEquals(nc.add(2, 4), 6);
+			
+		}
+		
+		@Test
+		public void addPos_Neg() {
+			assertEquals(nc.add(9, -5), 4);
+			
+		}
+		
+		@Test
+		public void addNeg() {
+			assertEquals(-2, nc.add(-1, -1));
+		}
+	}
 	
-//	@Test
-//	public void testDivide() {
-//		//testcase pass if it throws Exceptions.
-//		assertThrows(ArithmeticException.class, () -> nc.divide(1, 0), "Denominator should not be zero");
-//	}
+	
+	@Test
+	public void testMultiply() {
+		assertAll(
+				() -> assertEquals(0, nc.multiply(0, 1)),
+				() -> assertEquals(5, nc.multiply(5, 1)),
+				() -> assertEquals(nc.multiply(4, 5), 20)
+				);
+	}
+	
+	@Test
+	public void testDivide() {
+		//testcase pass if it throws Exceptions.
+		assertThrows(ArithmeticException.class, () -> nc.divide(1, 0), "Denominator should not be zero");
+	}
 	
 
 }
